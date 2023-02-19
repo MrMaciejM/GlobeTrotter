8ceba08abeb5bbe5d3bffaa35b319189121dbf6e
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-02-19 14:18:41 +0000

    removed old/commented code from Currency.js, added code to pull in an image for a city to Landing.js file

--- src/components/pages/Currency.js
+++ src/components/pages/Currency.js
@@ -13,19 +13,16 @@ import CurrencyConversionCard from "../CurrencyConvertCard";
 import RecentConversionsTable from "../RecentConversionsTable";
 import { setLocalStorage_RecentConversions } from "../helperFuncs/setLocalStorage_RecentConversions";
 
-// import "./Currency.css";
-//import { useState } from "react";
 // API key for APILayer currency:
 // hZn9Q1SDwhkak9rt1BHg0Iw018U8OgTl
 // to make a call
 // fetch("https://api.apilayer.com/fixer/convert?to=to&from=from&amount=amount", requestOptions)
 
-// IMPORTANT NOTE: while testing please be mindful of API calls - monthly limit is 100! I made < 15 calls. If we run out, sign up with an account on
+// IMPORTANT NOTE: while testing please be mindful of API calls - monthly limit is 100! I made < 20 calls. If we run out, sign up with an account on
 // https://fixer.io/
 // and update the API key.
 
 function Currency() {
-  var l = console.log;
   const [amount, setAmount] = useState("");
   const [countryFrom, setCountryFrom] = useState("");
   const [countryTo, setCountryTo] = useState("");
@@ -74,9 +71,6 @@ function Currency() {
     )
       .then((response) => response.json())
       .then((data) => {
-        l(data);
-        //l(result.info.rate);
-        //l(data.result);
         let resultRate = data.info.rate;
         let resultExchangedRate = data.result;
 
@@ -215,25 +209,6 @@ function Currency() {
             exRate={currencyRate || 0}
           />
         </Center>
-        {/* <div className="dashboardContainer">
-          <div>
-            <h5>Amount</h5>
-            <p>{amount}</p>
-          </div>
-          <div>
-            <h5>From</h5>
-            <p>{countryFrom}</p>
-          </div>
-          <div>
-            <h5>Rate</h5>
-            <p>{currencyRate}</p>
-          </div>
-          <div>
-            <h5>To</h5>
-            <p>{countryTo}</p>
-            <p>{exchangedRate}</p>
-          </div>
-        </div> */}
       </SimpleGrid>
       <Center mx="auto" my="10" maxW="800px">
         <RecentConversionsTable tableData={tableData} />
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,43 +1,46 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Landing() {
-  const [searchData, setSearchData] = useState(''); // user input
+  const [searchData, setSearchData] = useState(""); // user input
   const [geodata, setGeodata] = useState({}); //coords
   const [imageData, setImageData] = useState([]); // 5 images
   const [newsData, setNewsData] = useState([]); // 5 articles
   const [weatherData, setWeatherData] = useState({}); // info about right now
   const [countryData, setCountryData] = useState({}); // grabbing currency and language data
 
+  // URL to pull in single city image
+  const [imgUrl, setImgUrl] = useState("");
+
   // big useEffect hook for when geodata is grabbed
   useEffect(() => {
     // if there's no geodata, i.e. first render, then do nothing
     if (Object.keys(geodata).length === 0) {
-      console.log('no geodata!');
+      console.log("no geodata!");
       return;
     }
 
     // grab weather data
     const getWeatherData = async () => {
       const response = await axios.get(
-        'https://api.openweathermap.org/data/2.5/weather',
+        "https://api.openweathermap.org/data/2.5/weather",
         {
           params: {
-            appid: '0e8f67bf6ac0e37689d7edea5f37f808',
+            appid: "0e8f67bf6ac0e37689d7edea5f37f808",
             lat: geodata.lat,
             lon: geodata.lon,
-            units: 'metric',
+            units: "metric",
           },
         }
       );
-      console.log('weatherResponse', response.data);
+      console.log("weatherResponse", response.data);
       setWeatherData({
         ...weatherData,
-        ['icon']: response.data.weather[0].icon,
-        ['temp']: response.data.main.temp,
-        ['description']: response.data.weather[0].description,
-        ['timezone']: response.data.timezone,
-        ['timestamp']: response.data.dt,
+        ["icon"]: response.data.weather[0].icon,
+        ["temp"]: response.data.main.temp,
+        ["description"]: response.data.weather[0].description,
+        ["timezone"]: response.data.timezone,
+        ["timestamp"]: response.data.dt,
       });
     };
 
@@ -48,13 +51,13 @@ function Landing() {
       const response = await axios.get(
         `https://restcountries.com/v3.1/alpha/${countryData.countryCode}`
       );
-      console.log('getCountryDataResponse', response.data[0]);
+      console.log("getCountryDataResponse", response.data[0]);
 
       setCountryData({
         ...countryData,
-        ['flagSvgUrl']: response.data[0].flags.svg,
-        ['language']: Object.values(response.data[0].languages)[0],
-        ['currencyCode']: Object.keys(response.data[0].currencies)[0],
+        ["flagSvgUrl"]: response.data[0].flags.svg,
+        ["language"]: Object.values(response.data[0].languages)[0],
+        ["currencyCode"]: Object.keys(response.data[0].currencies)[0],
       });
     };
 
@@ -67,12 +70,12 @@ function Landing() {
         `https://www.travel-advisory.info/api?countrycode=${countryData.countryCode}`
       );
       console.log(
-        'travelAdvisoryResponse',
+        "travelAdvisoryResponse",
         response.data.data[capitalisedCountryCode].advisory.score
       );
       setCountryData({
         ...countryData,
-        ['travelAdvisoryScore']:
+        ["travelAdvisoryScore"]:
           response.data.data[capitalisedCountryCode].advisory.score,
       });
     };
@@ -81,18 +84,18 @@ function Landing() {
 
     // grab news data
     const getNews = async () => {
-      const response = await axios.get('https://newsapi.org/v2/everything', {
+      const response = await axios.get("https://newsapi.org/v2/everything", {
         params: {
           q: countryData.cityName,
-          apiKey: '66a24015f83f414aad84ea0d18eaaccd',
-          sources: 'bbc-news,cnn',
+          apiKey: "66a24015f83f414aad84ea0d18eaaccd",
+          sources: "bbc-news,cnn",
           pageSize: 5,
-          language: 'en',
-          sortBy: 'relevancy',
-          searchIn: 'content',
+          language: "en",
+          sortBy: "relevancy",
+          searchIn: "content",
         },
       });
-      console.log('newsResponse', response.data);
+      console.log("newsResponse", response.data);
       setNewsData(response.data.articles);
     };
 
@@ -101,48 +104,69 @@ function Landing() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('form submitted!');
+    console.log("form submitted!");
 
     if (searchData.length === 0) {
-      console.log('no text entered!');
+      console.log("no text entered!");
       return;
     }
 
     const getGeoData = async () => {
       const response = await axios.get(
-        'https://api.openweathermap.org/geo/1.0/direct',
+        "https://api.openweathermap.org/geo/1.0/direct",
         {
           params: {
-            appid: '0e8f67bf6ac0e37689d7edea5f37f808',
+            appid: "0e8f67bf6ac0e37689d7edea5f37f808",
             limit: 1,
             q: searchData,
           },
         }
       );
       if (response.data.length === 0) {
-        console.log('no result found!');
+        console.log("no result found!");
         return;
       }
       console.log(response.data[0]);
       setGeodata({
         ...geodata,
-        ['lat']: response.data[0].lat,
-        ['lon']: response.data[0].lon,
+        ["lat"]: response.data[0].lat,
+        ["lon"]: response.data[0].lon,
       });
       setCountryData({
         ...countryData,
-        ['cityName']: response.data[0].name,
-        ['countryCode']: response.data[0].country.toLowerCase(),
+        ["cityName"]: response.data[0].name,
+        ["countryCode"]: response.data[0].country.toLowerCase(),
       });
     };
 
-    console.log('searchData', searchData);
+    console.log("searchData", searchData);
 
     getGeoData();
 
-    console.log('geoData', geodata);
+    console.log("geoData", geodata);
 
-    setSearchData('');
+    // GET image of the city based on search input value
+    // uSplash API: https://api.unsplash.com/
+    //Access key: 6fEh-7tXr4Bzr12yxHDIN93ZeDnhTWDnRIZ49nxMRtY
+    //Secret Key: 6JpWEchohX37O7UTFdTJOSOzw-h2oWcJAMJ1MCvuL9Q
+    function getImage() {
+      const citySearch = searchData;
+      const clientID = "6fEh-7tXr4Bzr12yxHDIN93ZeDnhTWDnRIZ49nxMRtY";
+      const endpoint = `https://api.unsplash.com/search/photos?query=${citySearch}&per_page=1&client_id=${clientID}`;
+
+      fetch(endpoint)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          //console.log(data.results[0].urls.thumb);
+          setImgUrl(data.results[0].urls.thumb);
+        });
+    }
+    getImage();
+
+    setSearchData("");
   };
 
   const handleChange = (event) => {
@@ -155,13 +179,14 @@ function Landing() {
       <div>
         <form onSubmit={handleSubmit}>
           <input
-            type='text'
-            name='city'
-            placeholder='Enter city...'
+            type="text"
+            name="city"
+            placeholder="Enter city..."
             value={searchData}
             onChange={handleChange}
           />
         </form>
+        <img alt="city" src={imgUrl} />
         <p>search input: {searchData}</p>
         <p>latitude: {geodata.lat}</p>
         <p>longitude: {geodata.lon}</p>
@@ -180,8 +205,8 @@ function Landing() {
           <ul>
             {newsData.map((article, i) => (
               <li key={i}>
-                {article.title}{' '}
-                <a href={article.url} target='_blank'>
+                {article.title}{" "}
+                <a href={article.url} target="_blank">
                   link
                 </a>
               </li>
