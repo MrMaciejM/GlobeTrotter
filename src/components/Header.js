d6584500bdd79eb2dced3108d8f1a5c205d3884a
ssb <sb1664@gmail.com>
2023-02-20 14:56:50 +0000

    some animations test and small fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,36 @@
-
 import Sidebar from './Sidebar';
 
-import { Flex, Spacer, Box, Heading } from '@chakra-ui/react'
-
+import { Flex, Spacer, Box, Heading } from '@chakra-ui/react';
 
 function Header() {
-
   const stylesObj = {
     header: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      padding: "2px 5px"
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '2px 5px',
     },
     Sidebar: {
-      padding: "5px"
+      padding: '5px',
     },
-    h1: {
-
-    }
-  }
+    h1: {},
+  };
 
   return (
-    <Flex as="header" align="center" p="1">
-      <Box>
+    <Flex
+      as='header'
+      alignItems='center'
+      justifyContent='center'
+      p='1'
+      position='relative'
+      m={2}
+    >
+      <Box ml={2} position='absolute' left='5px'>
         <Sidebar />
       </Box>
-      <Box flex="1" textAlign="center">
-        <Heading>Travel App</Heading>
-      </Box>
+      <Heading as='h3' fontSize='30px'>
+        Travel App
+      </Heading>
     </Flex>
   );
 }
